6e6b8fd222ade3853b7adc01268126ddad229b7e
Anh Mac <anhmv91@gmail.com>
2021-05-09 11:36:06 +0700

    0.1.9

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Tradehub.MixProject do
       name: "Tradehub",
       app: :tradehub,
       description: "Tradehub SDK for Elixir",
-      version: "0.1.8",
+      version: "0.1.9",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
