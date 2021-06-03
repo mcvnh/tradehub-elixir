1989012376204214d13621cf0c754aa432aec56f
Anh Mac <anh@bespokify.com>
2021-06-03 10:10:39 +0700

    test(trade): cover connection errors

--- test/tradehub/trade_test.exs
+++ test/tradehub/trade_test.exs
@@ -1,4 +1,61 @@
 defmodule TradehubTest.TradeTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
   doctest Tradehub.Trade
+
+  setup do
+    env = Application.get_all_env(:tradehub)
+    on_exit(fn -> Application.put_all_env([{:tradehub, env}]) end)
+  end
+
+  test "expect failures" do
+    Application.put_env(:tradehub, :http_client, TradehubTest.NetTimeoutMock)
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.get_orders!("")
+    end
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.get_order!("")
+    end
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.positions!("")
+    end
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.positions_sorted_size!("swth_eth1")
+    end
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.positions_sorted_risk!("swth_eth1", "")
+    end
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.positions_sorted_pnl!("swth_eth1")
+    end
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.position!("", "swth_eth1")
+    end
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.position!("", "swth_eth1")
+    end
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.leverage!("", "swth_eth1")
+    end
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.trades!()
+    end
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.trades_by_account!("")
+    end
+
+    assert_raise HTTPoison.Error, fn ->
+      Tradehub.Trade.liquidations!()
+    end
+  end
 end
